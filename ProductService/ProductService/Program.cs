59e844108fdeec6b33c04070f69ea06cac5cd1d8
ASUS <idosgali@gmail.com>
2025-02-04 19:27:46 +0500

    MediatR За регестрировал

--- ProductService/ProductService/Program.cs
+++ ProductService/ProductService/Program.cs
@@ -1,14 +1,24 @@
+using System.Reflection;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProductService.Application.Commands;
 using ProductService.Infrastructure.Persistence;
 using ProductService.Infrastructure.Repositories;
 using ProductService.Domain.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Подключение базы данных PostgreSQL
 builder.Services.AddDbContext<ProductDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Регистрация репозитория
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+//Регистрация MediatR
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
